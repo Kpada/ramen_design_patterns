659611bcf124ebe01336cb407ee8f09190ff59b5
Nik Chizhov <chizhov.nik.s@gmail.com>
2023-07-23 14:56:35 +0900

    refactor: factory method

--- patterns/creational/factory_method/README.md
+++ patterns/creational/factory_method/README.md
@@ -1,21 +1,30 @@
 # Factory method pattern
 
-The factory method pattern is a creational pattern that uses factory methods to deal with the problem of creating objects without having to specify the exact class of the object that will be created. This is done by creating objects by calling a factory method—either specified in an interface and implemented by child classes, or implemented in a base class and optionally overridden by derived classes—rather than by calling a constructor.
+The Factory Method pattern is a creational design pattern that provides an
+interface for creating objects, but allows subclasses to alter the type of
+objects that will be created.
 
 When to use:
- - You don't know the types and dependencies of objects with which your code should work.
- - You want to enable users to extend parts of your framework or library
- - You want to save system resources by reusing already created objects instead of spawning new ones.
+
+* You don't know the types and dependencies of objects with which your
+code should work.
+* You want to enable users to extend parts of your framework orlibrary.
+* You want to save system resources by reusing already created objects instead
+of spawning new ones.
 
 But:
- - Can lead to the creation of a bunch of parallel class hierarchies. For each product class, you will need to create its creator subclass.
+
+* It can lead to the creation of a bunch of parallel class hierarchies. For each
+product class, you will need to create its creator subclass.
 
 ## Output
 
-It's dinner time. I'm so hungry. I don't know what exactly I want to eat. I'm just going to visit the closest restaurant and order their best meal.
+It's dinner time. I'm so hungry. I don't know what exactly I want to eat.I'm
+just going to visit the nearest restaurant and order their best meal.
 
-> [Me] Hello. I don't know that kind of restaurant is this. I don't know what dishes you have. Give me your best meal, please.
+> [Me] Hello. I don't know what type of restaurant this is. I don't know what
+dishes you serve. Give me your best meal, please.
 
-This is Ramen!
+This is Curry!
 
-> [Me] It was very tasty. Thanks.
+> [Me] That was very tasty. Thank you.
--- patterns/creational/factory_method/factoryMethod.h
+++ patterns/creational/factory_method/factoryMethod.h
@@ -1,60 +1,60 @@
-#ifndef __FACTORY_METHOD_H__
-#define __FACTORY_METHOD_H__
+#ifndef PATTERNS_CREATIONAL_FACTORY_METHOD_FACTORYMETHOD_H_
+#define PATTERNS_CREATIONAL_FACTORY_METHOD_FACTORYMETHOD_H_
 
 #include <exception>
 #include <iostream>
 #include <memory>
 #include <random>
+#include <utility>
 
 #include "../../iPattern.h"
 
-/* GoF design pattern: Factory method */
-
+// GoF design pattern: Factory method
 namespace FactoryMethod {
 
-/* Food interface */
+// Food interface
 class IFood {
  public:
   virtual ~IFood() = default;
-  virtual void Eat() const = 0;
+  virtual void Eat() const noexcept = 0;
 
  protected:
-  void SetUpText() const { std::cout << PrinterState::PlainText; }
+  void SetUpText() const noexcept { std::cout << PrinterState::PlainText; }
 };
 
-/* Contrete food 1 */
+// Concrete food 1
 class Ramen : public IFood {
  public:
-  void Eat() const override {
+  void Eat() const noexcept override {
     SetUpText();
     std::cout << "This is Ramen!\n";
   }
 };
 
-/* Contrete food 2 */
+// Concrete food 2
 class Sushi : public IFood {
  public:
-  void Eat() const override {
+  void Eat() const noexcept override {
     SetUpText();
     std::cout << "This is Sushi!\n";
   }
 };
 
-/* Contrete food 3 */
+// Concrete food 3
 class Curry : public IFood {
  public:
-  void Eat() const override {
+  void Eat() const noexcept override {
     SetUpText();
     std::cout << "This is Curry!\n";
   }
 };
 
-/* Creator */
+// Creator
 class IRestaurant {
  public:
   virtual ~IRestaurant() = default;
 
-  /* Creator may have some logic */
+  // Creator may have some logic
   void HaveDinner() const {
     std::unique_ptr<IFood> food = FactoryMethod();
     food->Eat();
@@ -64,7 +64,7 @@ class IRestaurant {
   virtual std::unique_ptr<IFood> FactoryMethod() const = 0;
 };
 
-/* Concrete Creator 1 */
+// Concrete Creator 1
 class RamenRestaurant : public IRestaurant {
  private:
   std::unique_ptr<IFood> FactoryMethod() const override {
@@ -72,7 +72,7 @@ class RamenRestaurant : public IRestaurant {
   }
 };
 
-/* Concrete Creator 2 */
+// Concrete Creator 2
 class SushiRestaurant : public IRestaurant {
  private:
   std::unique_ptr<IFood> FactoryMethod() const override {
@@ -80,7 +80,7 @@ class SushiRestaurant : public IRestaurant {
   }
 };
 
-/* Concrete Creator 3 */
+// Concrete Creator 3
 class IndianRestaurant : public IRestaurant {
  private:
   std::unique_ptr<IFood> FactoryMethod() const override {
@@ -88,7 +88,7 @@ class IndianRestaurant : public IRestaurant {
   }
 };
 
-/* Factory Method Pattern */
+// Factory Method Pattern
 class Pattern : public IPattern {
  public:
   Pattern() : IPattern("Factory Method") {}
@@ -98,35 +98,32 @@ class Pattern : public IPattern {
     std::cout
         << PrinterState::PlainText
         << "It's dinner time. I'm so hungry. I don't know what exactly "
-        << "I want to eat. I'm just going to visit the closest restaurant "
+        << "I want to eat. I'm just going to visit the nearest restaurant "
         << "and order their best meal.\n";
 
-    /* get a random restaurant */
+    // get a random restaurant
     std::unique_ptr<IRestaurant> restaurant = GetRandomRestaurant();
 
-    /* and visit it */
-    VisitRestaurant(std::move(restaurant));
+    // and visit it
+    VisitRestaurant(*restaurant);
   }
 
  private:
-  void VisitRestaurant(std::unique_ptr<IRestaurant> restaurant) const {
-    if (!restaurant) {
-      throw std::runtime_error("");
-    }
-
+  void VisitRestaurant(const IRestaurant& restaurant) const {
     std::cout << PrinterState::Quote
-              << "[Me] Hello. I don't know that kind of restaurant is this. "
-              << "I don't know what dishes you have. Give me your best meal, "
+              << "[Me] Hello. I don't know what type of restaurant this is. "
+              << "I don't know what dishes you serve. Give me your best meal, "
               << "please.\n";
 
-    restaurant->HaveDinner();
+    restaurant.HaveDinner();
 
-    std::cout << PrinterState::Quote << "[Me] It was very tasty. Thanks.\n";
+    std::cout << PrinterState::Quote
+              << "[Me] That was very tasty. Thank you.\n";
   }
 
-  /* Returns a random factory (restaurant) */
+  // Returns a random factory (restaurant)
   std::unique_ptr<IRestaurant> GetRandomRestaurant() const {
-    const unsigned int numOfRestaurants = 3;
+    static constexpr int numOfRestaurants{3};
 
     std::random_device rd;
     const int idx = rd() % numOfRestaurants;
@@ -134,20 +131,16 @@ class Pattern : public IPattern {
     switch (idx) {
       case 0:
         return std::make_unique<RamenRestaurant>();
-        break;
       case 1:
         return std::make_unique<SushiRestaurant>();
-        break;
       case 2:
         return std::make_unique<IndianRestaurant>();
-        break;
       default:
         throw std::runtime_error("Unsupported fabric index");
-        break;
     }
   }
 };
 
 }  // namespace FactoryMethod
 
-#endif /* __FACTORY_METHOD_H__ */
+#endif  // PATTERNS_CREATIONAL_FACTORY_METHOD_FACTORYMETHOD_H_
